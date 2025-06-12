b87cbbf38a2c6c6d3830a89cd8f3a896218be5e6
Ismail <157899145+IsmailAbdulkareem@users.noreply.github.com>
2025-06-12 11:53:55 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,16 +9,16 @@ import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserBut
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MCB Mustafa Contractor & Builder | Trusted Home Repair Services",
+  title: "Mustafa Builder & Contractor| Trusted Home Repair Services",
   description:
     "Reliable home construction, handyman, plumbing, electrical, and landscaping services. Book professional repair and building services with MCB today!",
   keywords:
     "Home repair, construction services, contractor, handyman, plumbing, electrical, landscaping, renovation, building contractor, Karachi home services",
   openGraph: {
-    title: "MCB Contractor & Builder - Expert Home Repair",
+    title: "Mustafa Builder & Contractor| Trusted Home Repair Services",
     description:
       "Find expert construction, plumbing, electric, and home renovation services at MCB. Affordable and reliable home solutions.",
-    url: "https://yourwebsite.com", // replace with your actual domain
+    url: "https://www.mustafabuilderanddeveloper.com.pk/", // replace with your actual domain
     siteName: "MCB Contractor & Builder",
     images: [
       {
