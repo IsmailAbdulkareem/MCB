72a591623fcf25f8a216acaf84e828afb0ce03eb
Ismail <157899145+IsmailAbdulkareem@users.noreply.github.com>
2025-06-20 20:24:11 +0500

    Update next-sitemap.config.js and make seo friendly

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,20 @@
-// next-sitemap.config.js
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.mustafabuilderanddeveloper.com.pk', // your site URL
-    generateRobotsTxt: true, // (optional)
-    changefreq: 'weekly',
-    priority: 0.7,
-    sitemapSize: 5000,
-  };
-  
+  siteUrl: 'https://www.mustafabuilderanddeveloper.com.pk',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: [], // You can add routes here to exclude them from sitemap
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://www.mustafabuilderanddeveloper.com.pk/sitemap.xml',
+    ],
+  },
+};
