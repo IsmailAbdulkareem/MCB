5ed33b5df6d1102cdb27de530276f9a589e47dd6
Ismail <157899145+IsmailAbdulkareem@users.noreply.github.com>
2025-06-25 12:25:21 +0500

    Updated sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,40 +4,40 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mustafabuilderanddeveloper.com.pk/',
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://mustafabuilderanddeveloper.com.pk/about',
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://mustafabuilderanddeveloper.com.pk/services',
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/services',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://mustafabuilderanddeveloper.com.pk/projects',
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/projects',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,
     },
     {
-      url: 'https://mustafabuilderanddeveloper.com.pk/contact',
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
     {
-        url: 'https://mustafabuilderanddeveloper.com.pk/faq',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.7,
-      },
+      url: 'https://www.mustafabuilderanddeveloper.com.pk/faq',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
   ];
 }
